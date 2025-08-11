0684b6f9857644ca98545e455a233e76061a323f
JuanToquica <juanestebantoquicagonzalez@gmail.com>
2025-08-11 01:44:31 -0500

    Seguimiento de camara

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform player;
+    public float smoothSpeed;
+    public Vector2 maxXPosition;
+    public Vector2 maxYPosition;
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        float targetXPosition = transform.position.x;
+        float targetYPosition = transform.position.y;
+
+        if (player.position.x < maxXPosition.y && player.position.x > maxXPosition.x)
+            targetXPosition = player.position.x;
+        else
+            targetXPosition = player.position.x <= maxXPosition.x? maxXPosition.x : maxXPosition.y;
+
+        if (player.position.y < maxYPosition.y && player.position.y > maxYPosition.x)
+            targetYPosition = player.position.y;
+        else
+            targetYPosition = player.position.y <= maxYPosition.x ? maxYPosition.x : maxYPosition.y;
+
+        Vector3 newPosition = new Vector3(targetXPosition, targetYPosition, -10);
+        transform.position = Vector3.Lerp(transform.position, newPosition, smoothSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Security;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +8,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float acceleration;
     [SerializeField] private float jumpForce;
     [SerializeField] private Animator animator;
+    [SerializeField] private Transform head;
+    [SerializeField] private Transform arms;
+    [SerializeField] private float armRotationOffset;
+    [SerializeField] private float maxHeadRotation;
     public PlayerInput playerInput;
     public bool isGrounded;
     public float raycastDistance;
@@ -25,17 +30,48 @@ public class PlayerController : MonoBehaviour
     {
         movement = Mathf.Clamp(Mathf.SmoothDamp(movement, moveInput, ref movementRef, acceleration), -1, 1);
         SetIsGrounded();
+
     }
 
     private void FixedUpdate()
     {
-        ApplyMovement();   
+        ApplyMovement();
+        Aim();
     }
 
     private void ApplyMovement()
     {
         if (moveInput != 0)
             rb.linearVelocity = new Vector2(movementSpeed * moveInput, rb.linearVelocity.y);
+    }
+
+    private void Aim()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;
+        Vector2 armDirection = mousePos -arms.position;
+        Vector2 headDirection;
+        float armOffset = armRotationOffset;
+        if (direction.normalized.x >= 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            headDirection = mousePos - head.position;
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            headDirection = head.position - mousePos;
+            armOffset = 90 + (90 - armRotationOffset);
+        }
+
+        float armAngle = Mathf.Atan2(armDirection.y, armDirection.x) * Mathf.Rad2Deg;
+        float headAngle = Mathf.Atan2(headDirection.y, headDirection.x) * Mathf.Rad2Deg;
+
+        arms.rotation = Quaternion.Euler(0, 0, armAngle + armOffset);
+        if (Mathf.Abs(headAngle) < maxHeadRotation)
+            head.rotation = Quaternion.Euler(0, 0, headAngle);
+        else
+            head.rotation = Quaternion.Euler(0, 0, Mathf.Sign(headAngle) * maxHeadRotation);        
     }
 
     private void SetIsGrounded()
@@ -54,15 +90,18 @@ public class PlayerController : MonoBehaviour
     public void OnMove(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
+        {
             moveInput = ctx.ReadValue<float>();
+            animator.SetBool("walking", true);
+        }
         if (ctx.canceled)
+        {
+            animator.SetBool("walking", false);
             moveInput = 0;
-    }
+        }
 
-    public void OnLook(int layerIndex)
-    {
-        
     }
-
 
 }
+
+
