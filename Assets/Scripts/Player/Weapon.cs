0e4a65ae7fc08bce04aaddafc6fcd17988202d10
JuanToquica <juanestebantoquicagonzalez@gmail.com>
2025-08-22 17:53:43 -0500

    MuzleFlashes importadas

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -6,6 +6,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] protected Transform player;
     [SerializeField] protected GameObject bulletPrefab;
     [SerializeField] protected WeaponData weaponData;
+    [SerializeField] protected Animator muzleFlashAnimator;
     [SerializeField] protected int weaponIndex;
     public Transform rightGripPoint;
     public Transform leftGripPoint;
@@ -20,6 +21,7 @@ public class Weapon : MonoBehaviour
 
     public virtual void Fire() 
     {
+        muzleFlashAnimator.SetTrigger("Shot");
         BulletControler bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation).GetComponent<BulletControler>();
         Vector3 direction = player.localScale.x == 1 ? firePoint.right : -firePoint.right;
         bullet.Initialize(firePoint.position, direction, projectileSpeed, damage);
