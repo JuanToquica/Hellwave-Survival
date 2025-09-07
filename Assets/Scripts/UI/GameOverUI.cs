e1f40514dfa9415cb44664d4e193e7860f5b3191
JuanToquica <juanestebantoquicagonzalez@gmail.com>
2025-09-06 21:32:51 -0500

    GameOverUI Programada

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -1,16 +1,29 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Button retryButton;
+    [SerializeField] private Button exitButton;
+
+
+    private void Start()
     {
-        
+        retryButton.onClick.AddListener(OnRetryButtonClicked);
+        exitButton.onClick.AddListener(OnExitButtonClicked);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnRetryButtonClicked()
     {
-        
+        Time.timeScale = 1;
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+
+    public void OnExitButtonClicked()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadSceneAsync("MainMenu");
     }
 }
