5917bd4351fb2ae1206bdf9d8c74776672b8acc1
JuanToquica <juanestebantoquicagonzalez@gmail.com>
2025-09-09 12:10:58 -0500

    Spawner de cajas de municion

--- Assets/Scripts/AmmunitionSpawner.cs
+++ Assets/Scripts/AmmunitionSpawner.cs
@@ -1,7 +1,33 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AmmunitionSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject ammunitionBoxPrefab;
     [SerializeField] private Transform[] spawners;
+    [SerializeField] private float minTimeToSpawn;
+    [SerializeField] private float maxTimeToSpawn;
+
+    private void Start()
+    {
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            StartCoroutine(spawnAmmunitionBox(i));
+        }
+    }
+
+    public void BoxCollected(int spawnIndex)
+    {
+        StartCoroutine(spawnAmmunitionBox(spawnIndex));
+    }
+
+    private IEnumerator spawnAmmunitionBox(int spawnIndex)
+    {
+        yield return new WaitForSeconds(Random.Range(minTimeToSpawn, maxTimeToSpawn));
+        AmmunitionBox box = GameObject.Instantiate(ammunitionBoxPrefab, spawners[spawnIndex].position, spawners[spawnIndex].rotation).
+            GetComponent<AmmunitionBox>();
+        box.spawnIndex = spawnIndex;
+        box.spawner = this;
+    }
 }
--- Assets/Scripts/Weapons/AmmunitionBox.cs
+++ Assets/Scripts/Weapons/AmmunitionBox.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class AmmunitionBox : MonoBehaviour
 {
+    public AmmunitionSpawner spawner;
+    public int spawnIndex;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
             AudioManager.instance.PlayReloadSound();
+            spawner.BoxCollected(spawnIndex);
             Destroy(gameObject);
         }
     }
